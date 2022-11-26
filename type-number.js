3319baa787bd52c2400743e6e089068fe5f432dc
ThadeuLucio <thadeurocha90@gmail.com>
2022-11-26 10:39:39 -0300

    exercicio

---
+++ type-number.js
@@ -0,0 +1,17 @@
+//type number
+
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log(operacaoMatematica);
+
+//Numeros flutuantes
+
+const numeroPontoFlutuante = 3.3
+const pontoFlutuanteSemZero = .5
+
+const novaOperacao = numeroPontoFlutuante / pontoFlutuanteSemZero;
+console.log(novaOperacao);
+
+// NAN acontece quando tentamos fazer uma operação com algo que não é numero!
